ebd2c1d939ed2c21016192740b5bf01b173bb221
procoder102 <keshavcoder81@gmail.com>
2025-07-21 18:42:17 +0530

    Work on About Page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,45 @@
-
+import Image from 'next/image'
+import gifFile from '../../Images/web-design-gif--final.gif'
+import iconW from '../../Images/icon-web-design-20.jpg'
 
 export default function AboutUs(){
   return (
-    <div>page</div>
+    <div className=''>
+      <section className="p-30">
+        <div className="flex flex-col md:flex-row  justify-between" >
+          <div className='w-1/2'>
+            <h1 className='text-center font-bold text-3xl'>THE BRANDS DOOR</h1>
+            <p className='pt-3 font-samibold font-serif text-center text-gray-500'>
+              The essence of The Brands Door is to thrive together in business with their clients. We are a trustworthy vendor for a vast number of clients. We ensure that our clients get what they want. We work closely with a range of clients from different sectors and regions across the nation. We at The Brands Door have certified experts and employs with advanced technologies delivering top-notch digital commerce solutions. We aid businesses in finding suitable candidates and workforce who could be an asset to the company. We are also involved into state-of-the-art web design & development to empower you to grow with flawless ease. Doesn’t matter whether you are startup with a small projector an, enterprise needing an agile development team.
+            </p>
+          </div>
+          <div className='w-1/3'>
+              <Image src={gifFile} unoptimized={true} alt='...' />
+          </div>
+        </div>
+      </section>
+      <section className='px-10'>
+        <h1 className=''>THE BRANDS DOOR GROUP UNIQUE VALUES</h1>
+        <h1 className='pt-5 font-bold'>INVITING STYLE</h1>
+        <p className='pt-3'>We believe every function deserves a well-designed form. Our products offer a style that is personal, approachable, and satisfying. Fitting perfectly into your life.</p>
+        <h1 className='pt-5 font-bold'>BUILT TO LAST</h1>
+        <p className='pt-3'>From the materials we use to the care we put into building them, our products are made to stand the test of time. It’s a quality you can depend on today and be confident in for a lifetime.</p>
+        <h1 className='pt-5 font-bold'>SUPERIOR PERFORMANCE</h1>
+        <p className='pt-3'>For the past 25 years, THE BRANDS DOOR GROUP has set and reset the standard for products that work and wow. We build worry-free performance into every product we make.</p>
+        <h1 className='pt-5 font-bold'>PURPOSEFUL FUNCTION</h1>
+        <p className='pt-3'>Our products are designed to make life healthier, safer, easier, and more beautiful while contributing to the efficient use of water and the sustainable development of society.</p>
+      </section>
+      <section className=''>
+        <div className='flex'>
+          <div>
+            <Image src={iconW}  alt='...' />
+          </div>
+          <div>
+            <h1>Green Innovation</h1>
+            <p>Green – new products and technologies which provide customer and business value but significantly decrease environmental impacts.</p>
+          </div>
+        </div>
+      </section>
+    </div>
   )
 }
